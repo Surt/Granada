b4dc3a9ef08ca4b5a251adae2b44081bd65edbea
Erik Wiesenthal <erikforo@hotmail.com>
2013-11-07 18:05:01 +0100

    fix test to include by hand.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,5 +1,11 @@
 <?php
 
-include __DIR__ . '/../vendor/autoload.php';
+include __DIR__ . '/../src/Granada/ORM.php';
+include __DIR__ . '/../src/Granada/Orm/Wrapper.php';
+include __DIR__ . '/../src/Granada/Orm/String.php';
+include __DIR__ . '/../src/Granada/Granada.php';
+include __DIR__ . '/../src/Granada/Eager.php';
+include __DIR__ . '/../src/Granada/Model.php';
+include __DIR__ . '/../src/Granada/ResultSet.php';
 include __DIR__ . '/MockPDO.php';
 include __DIR__ . '/models.php';
