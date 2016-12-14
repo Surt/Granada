550ebee7e24adb16059a00c68dfc417955c37f76
Patrick Roozeboom <proozeboom91@gmail.com>
2016-12-14 09:52:46 +0100

    Patched Orm\String to Orm\Str for PHP7.0 compatibility.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,7 +2,7 @@
 
 include __DIR__ . '/../src/Granada/ORM.php';
 include __DIR__ . '/../src/Granada/Orm/Wrapper.php';
-include __DIR__ . '/../src/Granada/Orm/String.php';
+include __DIR__ . '/../src/Granada/Orm/Str.php';
 include __DIR__ . '/../src/Granada/Granada.php';
 include __DIR__ . '/../src/Granada/Eager.php';
 include __DIR__ . '/../src/Granada/Model.php';
